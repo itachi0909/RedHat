e593024975b0bfc103fa2d680dcd7e1c16233ef8
Pratham Joshi <87461737+prathamjoshi0909@users.noreply.github.com>
2023-12-01 16:52:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,14 @@
+from flask import Flask
+import os
 
-# Function to add two numbers
-def add(x, y):
-    return x + y
+app = Flask(__name__)
 
-# Function to subtract two numbers
-def subtract(x, y):
-    return x - y
+@app.route('/')
+def hello():
+    return "Hello World!"
 
-# Function to multiply two numbers
-def multiply(x, y):
-    return x * y
+if __name__ == '__main__':
+    port = os.environ.get('FLASK_PORT') or 8080
+    port = int(port)
 
-# Function to divide two numbers
-def divide(x, y):
-    if y == 0:
-        return "Cannot divide by zero"
-    return x / y
-
-# Main program
-while True:
-    print("Options:")
-    print("Enter 'add' for addition")
-    print("Enter 'subtract' for subtraction")
-    print("Enter 'multiply' for multiplication")
-    print("Enter 'divide' for division")
-    print("Enter 'quit' to end the program")
-    
-    user_choice = input(": ")
-    
-    if user_choice == "quit":
-        break
-    
-    if user_choice in ("add", "subtract", "multiply", "divide"):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-        
-        if user_choice == "add":
-            print("Result:", add(num1, num2))
-        elif user_choice == "subtract":
-            print("Result:", subtract(num1, num2))
-        elif user_choice == "multiply":
-            print("Result:", multiply(num1, num2))
-        elif user_choice == "divide":
-            print("Result:", divide(num1, num2))
-    else:
-        print("Invalid input")
+    app.run(port=port,host='0.0.0.0')
